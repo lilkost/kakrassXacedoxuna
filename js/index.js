896f9bacf8ad259171ccf857ab06799df4495ea9
lilkost <111429255+lilkost@users.noreply.github.com>
2024-11-09 12:13:39 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,85 @@
+// включение бегущей строки
+new Marquee('marquee', {
+    speed: .4 // default: .5
+});
+// активация модалок с видео
+Fancybox.bind("[data-fancybox]", {
+    // Ваши настраиваемые параметры
+  });
+// активация слайдера
+const swiper = new Swiper('.rewiew__slider', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    centeredSlides: false,
+
+    breakpoints: {
+        481: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            centeredSlides: false
+        },
+        280:{
+            loop: true,
+            slidesPerView: 2,
+            centeredSlides: false,
+            spaceBetween: 15,
+            initialSlide: 2,
+        }
+    }
+});
+// открытие/скрытие аккардеона
+if(document.querySelector(".program__accordion")) {
+    const accordion = document.querySelectorAll('.program__accordion');
+
+    accordion.forEach(acc=> {
+        const top = acc.querySelector('.program__accordion-top');
+
+        top.addEventListener("click", ()=>{
+            acc.classList.toggle("is-active")
+        })
+    })
+}
+// бургер меню
+if(document.querySelector('.burger-btn')) {
+    const burgerBtn = document.querySelector(".burger-btn");
+    const header = document.querySelector(".header");
+
+    burgerBtn.addEventListener("click", ()=> {
+        header.classList.toggle("is-active");
+    })
+}
+// позиционирование бегущей строки
+
+if(document.querySelector(".top__box")) {
+    document.addEventListener("DOMContentLoaded", ()=> {
+        const topBox = document.querySelector(".top__box");
+        const textNode = document.querySelector(".top__texts");
+        const paragraphNodes = textNode.querySelectorAll('p');
+        const marquee = document.querySelector(".top__marquee");
+    
+    
+    
+        const setPositionBLock = () => {
+            const size = {
+                parent: topBox.clientHeight,
+                last: paragraphNodes[paragraphNodes.length - 1].clientHeight
+            }
+    
+            let posY = size.parent - size.last;
+    
+            console.log(size, posY)
+    
+            marquee.style = ` --posY: ${posY}px; overflow: hidden;`;
+        }
+    
+        setPositionBLock();
+    
+        window.addEventListener("resize", ()=> {
+            setPositionBLock();
+        })
+    })
+
+}
